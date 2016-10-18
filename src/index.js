d4df6461c962701350bc81461e3a0a23eb9dbae1
JeseWang <jessewang1024@gmail.com>
2016-10-18 18:22:32 +0800

    'demo'

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-const Main = {template: '#main'};
-const OpenIndex = {template: '#openIndex'};
-const AddIndex = {template: '#addIndex'};
-
-const routes = [
-	{path: '/main',component: Main},
-	{path: '/openIndex',component: OpenIndex},
-	{path: '/addIndex',component: AddIndex}
-];
-
-const router = new VueRouter({
-	routes
-});
-
-const app = new Vue({
-	router
-}).$mount('#app');
+var vm = new Vue({
+ 	el: '#app',
+ 	ready:function(){
+ 		$.get('https://api.myjson.com/bins/r8mm',function(data){
+ 			vm.items = data;
+ 		})
+ 	},
+ 	data:{
+ 		signIn:false,
+ 		items: [],
+ 		item:{
+ 			id:'',
+ 			name:'',
+ 			date:'',
+ 			index:''
+ 		}
+ 	},
+ 	computed:{
+ 		
+ 	},
+ 	methods:{
+ 		addIndex:function(item){
+ 			this.items.push(this.item);
+ 			this.item = '';
+ 		},
+ 		deleteIndex:function(item){
+ 			this.items.$remove(item)
+ 		},
+ 		changeIndex:function(item){
+ 			str = $(event.target).text()=="修改"?"保存":"修改"
+ 			$(event.target).text(str);
+ 			if($(event.target).text()=="保存"){
+ 				$(event.target).parents('tr').find('input').removeAttr('disabled');
+ 			}else{
+ 				$(event.target).parents('tr').find('input').attr('disabled','disabled');
+ 			}
+ 		},
+ 		signIndex:function(){
+ 			this.signIn = true;
+ 		},
+ 		openIndex:function(){
+ 			$('#eCharts').show()
+ 		},
+ 		close:function(){
+ 			$('#eCharts').hide()
+ 		},
+ 		upData:function(){
+ 			location.reload();
+ 		}
+ 	}
+ })
---
+++ src/showCharts.js
@@ -0,0 +1,52 @@
+(function(){
+	var myChart = echarts.init(document.getElementById('showCharts'));
+	var data = [];
+	var now = +new Date(2016,0,0);
+	var oneDay = 24 * 3600 * 1000;
+	var value = Math.random()*1000;
+	for (var i = 0; i < 1000; i++){
+		data.push(randomData());
+	}
+
+	function randomData(){
+		now = new Date(+now + oneDay);
+		return{
+			name: now.toString(),
+			value:[
+				[now.getFullYear(),now.getMonth()+1,now.getDate()].join('/')
+			]
+		};
+	}
+
+	var option = {
+		title:{
+			text:'玻璃指数折线图'
+		},
+		tooltip:{},
+		legend:{
+			data:['期初指数']
+		},
+		dataZoom:[
+			{
+				type:'slider',
+				start: 10,
+				end: 60
+			},
+			{
+				type:'inside',
+				start:10,
+				end:60
+			}
+		],
+		xAxis:{
+			data:data
+		},
+		yAxis:{},
+		series:[{
+			name:'期初指数',
+			type:'line',
+			data:[50,70,90,40,60,10,100,90,50,70,50,70,90,40,60,10,100,90,50,70,50,70,90,40,60,10,100,90,50,70,50,70,90,40,60,10,100,90,50,70,50,70,90,40,60,10,100,90,50,70,50,70,90,40,60,10,100,90,50,70,50,70,90,40,60,10,100,90,50,70,50,70,90,40,60,10,100,90,50,70,90,40,60,10,100,90,50,70,90,40,60,10,100,90,50,70,90,40,60,10,100,90,50,70,90,40,60,10,100,90,50,70,90,40,60,10,100,90,50,70,90,40,60,10,100,90,50,70,90,40,60,10,100,90,]
+		}]
+	};
+	myChart.setOption(option);	
+})()
